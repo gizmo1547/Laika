66c6a4c79059459868c124f0dceda12956cf206b
rivaldo007 <55262172+rivaldo007@users.noreply.github.com>
2025-11-26 04:51:30 -0500

    Delete MainFilesIEEE802.11p directory

--- MainFilesIEEE802.11p/resumeBackoff11p.m
+++
@@ -1,6 +0,0 @@
-function timeNextTxRx = resumeBackoff11p(timeNow,nSlotBackoff,tAifs,tSlot)
-% The backoff is resumed after having been freezed
-
-timeNextTxRx = round(timeNow + tAifs + nSlotBackoff * tSlot, 10);
-
-end
